75df8daf18d053dd04eb05a88a72fbdca2cf964e
Luam F Menezes <luamfmenezes@gmail.com>
2020-09-18 10:48:37 -0300

    anagrams and frequency counter

---
+++ jsfunctions.js
@@ -0,0 +1,5 @@
+"teste".indexOf("e"); // return position of first 'e' in the string;
+"teste".indexOf("a"); // return -1
+
+[1, 2, 3, 4, 5].splice(2, 1); // [1,2,4,5]
+[1, 2, 3, 4, 5].splice(1, 3); // [1,5]
